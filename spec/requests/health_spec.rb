fcca068a9fcc404f494e7ef2a2bedac9dc85c335
rbd3 <rabedesanaa@gmail.com>
2023-09-25 18:39:20 +0300

    Add testing environment

---
+++ spec/requests/health_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Healths", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/health/index"
+      expect(response.body).to eq('{"status":"online"}')
+      expect(response).to have_http_status(:success)
+     #expect(response.status).to eq(200)
+    end
+  end
+
+end
